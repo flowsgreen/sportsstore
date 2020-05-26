2d4b310984b753150781c038bb196738f803ee55
Majek Olaitan <majekolaitan@Olaitans-MacBook.local>
2020-05-26 02:12:29 +0100

    5.4.2. Created the Cart Summary Component

--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CategoryNavigation } from "./CategoryNavigation";
 import { ProductList } from "./ProductList";
+import { CartSummary } from "./CartSummary";
 
 export class Shop extends Component {
   render() {
@@ -8,7 +9,8 @@ export class Shop extends Component {
       <div className="container-fluid">
         <div className="row">
           <div className="col bg-dark text-white">
-            <div className="navbar-brand">SPORTS STORE</div>{" "}
+            <div className="navbar-brand">SPORTS STORE</div>
+            <CartSummary {...this.props} />
           </div>
         </div>
         <div className="row">
