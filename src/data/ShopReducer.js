ae33d8d248f86cdcd47d799041aff5f2bca99d4b
Majek Olaitan <majekolaitan@Olaitans-MacBook.local>
2020-05-28 21:55:11 +0100

    6.2.2. HTTP Req and Action to include req parameters

--- src/data/ShopReducer.js
+++ src/data/ShopReducer.js
@@ -3,7 +3,12 @@ import { ActionTypes } from "./Types";
 export const ShopReducer = (storeData, action) => {
   switch (action.type) {
     case ActionTypes.DATA_LOAD:
-      return { ...storeData, [action.payload.dataType]: action.payload.data };
+      return {
+        ...storeData,
+        [action.payload.dataType]: action.payload.data,
+        [`${action.payload.dataType}_total`]: action.payload.total,
+        [`${action.payload.dataType}_params`]: action.payload.params,
+      };
     default:
       return storeData || {};
   }
